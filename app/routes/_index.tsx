2a581c6db4e6ca424eff0015888c4deb58146e05
jay <jay@jeepies.codes>
2025-05-14 10:28:44 +0100

    feat(ui): add landing page with login and register buttons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,22 @@
+import { Link } from '@remix-run/react'
+import { Button } from '~/components/ui/button'
+
 export default function Index() {
-  return <div className="flex h-screen items-center justify-center"></div>
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-900 mb-8">Lifeplan</h1>
+      <div className="flex gap-4">
+        <Link to="/login">
+          <Button >
+            Login
+          </Button>
+        </Link>
+        <Link to="/register">
+          <Button>
+            Register
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
 }
