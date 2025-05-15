39bd0152cef4a2053c8bb41b0b155883f3c62a95
jay <jay@jeepies.codes>
2025-05-15 16:52:15 +0100

    fix(sidebar): update app title from Lifeplan to Oyl

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -27,7 +27,7 @@ export function Sidebar({ className, user }: SidebarProps) {
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight text-gray-900 dark:text-gray-100">
-            Lifeplan
+            Oyl
           </h2>
           <Separator className="my-4" />
           <div className="space-y-1">
