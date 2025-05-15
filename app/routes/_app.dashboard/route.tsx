a0608be463a2236e35e349091fdd5a8f1dcc16d8
jay <jay@jeepies.codes>
2025-05-15 15:11:32 +0100

    feat(dashboard): load and display current user info

--- app/routes/_app.dashboard/route.tsx
+++ app/routes/_app.dashboard/route.tsx
@@ -1,3 +1,16 @@
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { getSession, getUserBySession } from '~/lib/session.server';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get('Cookie'));
+  const user = await getUserBySession(session);
+
+  return user!;
+}
+
 export default function Dashboard() {
-  return <></>;
+  const user = useLoaderData<typeof loader>();
+
+  return <>{user.username}</>;
 }
